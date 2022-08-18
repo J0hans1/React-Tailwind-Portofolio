7d974279f9662c64251f2027634eb81b77642b54
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-08-18 14:14:08 +0200

    v4

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -8,7 +8,7 @@ export default FrontPage;
 //pagesvar1
 function FrontPage(){
     return (
-        <div className="w-full h-screen m-0 bg-gradient-to-b from-purple-100 to-blue"> 
+        <div className="w-full m-0 bg-gradient-to-b from-purple-100 to-blue"> 
             <div className='flex justify-center mx-5 sm:mx-10 pt-5 sm:pt-36 md:pt-56 pb-40'>
                 <div className="flex flex-col items-center sm:w-3/4 lg:w-2/5 h-fit">
                     <TitledBox title="Welcome to my portfolio!" text="Hello, I am Markus Johansen. I am a student of information technologies at the Norwegian University of  Science and Technology (NTNU), Based In Trondheim, Norway. I made this website because i wanted a 'web based' portfolio and an opportunity to accummulate skills within industry-relevant frameworks (React and Tailwind). The portfolio will contain relevant information about me, my education, skills, and work experience."/>
