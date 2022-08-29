674a5c5ce00e532702f5fc1fe85863f4f8128c30
Markus Aleksander Råkil Johansen <markusrj@stud.ntnu.no>
2022-08-29 12:07:38 +0200

    got rid of page 2

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import SocialLinks from './components/buttons/SocialLinks';
 import WaveTransition from './components/WaveTransition';
 import Top from './components/pageTop';
 
-import SecondPage from './components/pages/Page2';
+//page 2 imports
+import logo from './img/ntnu-logo.png';
+import Topics from './components/buttons/TopicBox';
+import { useRef } from 'react';
+
 import MiddlePage from './components/pages/Page3';
 import FourthPage from './components/pages/Page4';
 
 function App() {
+  const inputRef = useRef()
+
   return (
     //page 1
     <div className="App overflow-hidden">
@@ -40,7 +46,38 @@ function App() {
               {/*transition */}
               <WaveTransition color="#FFFCF3"/>
           </div>
-      <SecondPage />
+
+
+          {/*page 2 */}
+          <div ref={inputRef} className="relative w-full h-fit m-0 bg-white-ivory">
+            {/*content */}
+            <div className='flex flex-col lg:justify-evenly mx-5 sm:mx-20 pt-4'>
+
+                {/*top row */}
+                <div className='flex flex-col xl:flex-row justify-evenly'>
+                    <div className="w-full xl:w-2/5 h-fit">
+                        <TitledBox title="About me" text="I have always had a facination for new technology, especially regarding computer technology, which is my reasoning for studying informatics. What i enjoy about this field of technology, is the ability to both be creatively expressive while staying in touch with advanced logical tasks. Within the field of IT, i am most experienced within software- and webdevelopement, but intrigued by topics like data-analytics, cloud computing, AI, Machine-learning and cybersecurity. In my sparetime i enjoy fly-fishing, playing guitar, hanging out with friends or noodling with some dev-projects of my own."/>
+                    </div>
+                    <div className="mx-auto xl:mx-0 w-full sm:w-7/12 xl:w-2/5 h-fit"> {/*!center the image */}
+                        <img className='mt-16' src={logo} alt="NTNU logo"/> 
+                    </div>
+                </div>
+
+                {/*bottom row */}
+                <div className="flex flex-col xl:flex-row-reverse sm:mt-16 lg:mt-20 justify-evenly">
+                    <div className="w-full xl:w-2/5 h-fit">
+                        <TitledBox title="Skillset and technologies" text="I have worked with the following technologies in school or as a part of my sparetime software-development. I am currently most skilled within Java and JavaScript programming, and i feel passionate about the entire stack of development (front- and backend). While this page is IT-focused, i have also included some skillsets which was important during my previous employments."/>
+                    </div>
+                    <div className="w-full xl:w-2/5 h-fit">
+                        <Topics/>
+                    </div>
+                </div>
+            </div>
+
+
+            {/*transition */}
+            <WaveTransition color="#A29BFE"/>
+        </div>
       <MiddlePage />
       <FourthPage />
       {/*<LastPage />*/}
